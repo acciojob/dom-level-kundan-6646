a0ba2af3d71018d05bf347ee0589595d16f44e55
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-04-15 15:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const item = document.getElementById('level');
+const allItems = document.querySelectorAll('li');
+
+let count = 0;
+
+for (let ele of allItems) {
+	count++;
+	if(ele === item) break;
+}
+
+alert('The level of the element is: ' + count);
